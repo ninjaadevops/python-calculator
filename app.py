2d38354881119ed495a547d5c2e02b98f216eec0
Rafiya Khan <ninjaadevops@gmail.com>
2025-05-05 21:47:58 -0300

    Update app.py

--- app.py
+++ app.py
@@ -14,4 +14,4 @@ def calculator():
     return render_template('index.html', result=result)
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=8080)
